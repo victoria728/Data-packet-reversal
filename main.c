4aa3e2862938d5d72c419c64c5a5de5b53f077eb
victoria728 <adaeze2.anyanwu@live.uwe.ac.uk>
2026-02-02 12:32:01 +0000

    commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   int sensorID = 99;
+   float temperature = 12.34;
+   char status_code = 'A';
+   printf("SensorID:%d,sensorID\n",sensorID);
+   printf("Temperature:%.2f,temperature\n",temperature);
+   printf("status code:%c,status_code\n",status_code);
+
+    return 0;
+}
